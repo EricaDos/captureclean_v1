ccc8645df7bbcde1ffc8748340c14dbc23069ab9
Dos <ericadossantos2277@gmail.com>
2019-11-18 13:11:39 +0000

    Updated code

--- Desktop/eclipse-workspace/Student/src/student.java
+++ Desktop/eclipse-workspace/Student/src/student.java
@@ -1,9 +1,58 @@
 
 public class student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String name;
+	int studentId;
+	int yearOfStudy;
+	double examMark;
+	String result;
+	
+	//Getters 
+	void setName(String n) {
+		name = n;
 	}
+	void setStudentId(int id) {
+		studentId = id;
+	}
+	void setYearOfStudy(int y) {
+		yearOfStudy = y;
+	}
+	void setExamMark(double m) {
+		examMark = m;
+	}
+	void setResult(String r) {
+		result = r;
+	}
+	
+	//Getters - Return the information of the class
+	
+	String getName() {
+		return name;
+	}
+	int getStudentId() {
+		return studentId;
+	}
+	int getYearOfStudy() {
+		return yearOfStudy;
+	}
+	double getExamMark() {
+		return examMark;
+	}
+	String getResult() {
+		return result;
+	}
+	
+	void finalResult() {
+		if(this.examMark >= 40) this.result = "Congrats, You Passed";
+		else this.result= "Sorry, You Failed";
+	}
+	public String toString(){
+		return "Name: "+ name + "Student Id: " + studentId + "Year: " + yearOfStudy + "Exam Mark: " + examMark + "Result :" + result;
+	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		
+//	}
+	
 
 }
